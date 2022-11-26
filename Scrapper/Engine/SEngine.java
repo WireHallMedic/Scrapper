fad5715b3d75bccfc09122e0a17c55a02fa98241
Michael <MichaelWidler@yahoo.com>
2022-11-26 14:28:02 -0800

    Add actorList

--- Scrapper/Engine/SEngine.java
+++ Scrapper/Engine/SEngine.java
@@ -2,16 +2,40 @@ package Scrapper.Engine;
 
 import Scrapper.Actor.*;
 import Scrapper.Map.*;
+import java.util.*;
 
 public class SEngine
 {
    private static SActor player = null;
    private static Zone curZone = null;
+   private static Vector<SActor> actorList = new Vector<SActor>();
    
    public static void setPlayer(SActor p){player = p;}
    public static void setCurZone(Zone z){curZone = z;}
+   public static void setActorList(Vector<SActor> al){actorList = al;}
    
    public static SActor getPlayer(){return player;}
    public static Zone getCurZone(){return curZone;}
+   public static Vector<SActor> getActorList(){return actorList;}
+   
+   
+   // actor methods
+   public static void add(SActor actor)
+   {
+      if(actorList.contains(actor) == false)
+      {
+         actorList.add(actor);
+      }
+   }
+   
+   public static void remove(SActor actor)
+   {
+      actorList.remove(actor);
+   }
+   
+   public static void clearActorList()
+   {
+      actorList = new Vector<SActor>();
+   }
    
 }
