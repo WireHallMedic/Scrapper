2241563bad764900f4edb2c6e77443cb152f2be3
Michael <MichaelWidler@yahoo.com>
2022-12-03 20:02:06 -0800

    Move ExitInfo to public class file.

---
+++ Scrapper/Map/ExitInfo.java
@@ -0,0 +1,14 @@
+package Scrapper.Map;
+
+// a small struct for passing exit information
+public class ExitInfo
+{
+   public int index;
+   public String name;
+   
+   public ExitInfo(int i, String n)
+   {
+      index = i;
+      name = n;
+   }
+}
